f7bee0b2b4d66ea8e4dd5a05900ff2520104bfec
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2020-06-01 16:08:56 +0200

    Add files via upload

---
+++ some_modules.py
@@ -0,0 +1,25 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 10:07:53 2020
+
+@author: najmeh
+"""
+
+
+
+def add(a, b, c):
+    return a + b + c
+
+def subtracr(a, b=1, c=10):
+      return a - b - c
+
+def devide(a, b=2, c=1):
+      return (a / c) / b
+
+
+def multiply(a, b=1, c=1):
+      return devide(a)*3
+
+
+
