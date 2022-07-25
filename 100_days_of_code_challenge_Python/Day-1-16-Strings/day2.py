5e746a2dc73d202ed212d477bcab947270d80723
Najmeh Foroozani <najmeh.foroozani@gmail.com>
2022-07-25 17:45:22 +0200

    Add files via upload

---
+++ 100_days_of_code_challenge_Python/Day-1-16-Strings/day2.py
@@ -0,0 +1,18 @@
+"""
+Write a Python program that prints the character at index i in the string s.
+If the index is out of range, the program should print "i is out of range"
+If the string is empty, the program should print "Empty String"
+"""
+##
+a = 'Hello'
+i = 2
+
+if a:
+    print(a[i])
+elif a == 0:
+    print('Empty String')
+else:
+    print('i is out of range')
+
+
+
