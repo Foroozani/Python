308cb6cd99fb06102bb30f0be5866c5c4e7d9eb2
Najmeh Foroozani <najmeh.foroozani@gmail.com>
2022-07-26 17:24:21 +0200

    Update day3.py

--- 100_days_of_code_challenge_Python/Day-1-16-Strings/day3.py
+++ 100_days_of_code_challenge_Python/Day-1-16-Strings/day3.py
@@ -10,7 +10,7 @@ def reverse_a_string(x):
     reversed_word = x[::-1]
     return reversed_word
 
-reverse_a_string('hello najmeh')
+reverse_a_string('hello word')
 ##
 """
 Write a Python Program that prints the reversed version of a string with charachter '-' in between.
