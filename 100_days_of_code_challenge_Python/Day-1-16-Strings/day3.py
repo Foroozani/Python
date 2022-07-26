df58691acceac11d44afca8b68447a4e14ef66a5
Najmeh Foroozani <najmeh.foroozani@gmail.com>
2022-07-26 17:23:49 +0200

    Add files via upload

---
+++ 100_days_of_code_challenge_Python/Day-1-16-Strings/day3.py
@@ -0,0 +1,25 @@
+"""
+Write a Python Program that prints the reversed version of a string.
+The program must preserve uppercase and lowercase letters.
+"""
+
+s = 'Hello'
+print(s[::-1])
+##
+def reverse_a_string(x):
+    reversed_word = x[::-1]
+    return reversed_word
+
+reverse_a_string('hello najmeh')
+##
+"""
+Write a Python Program that prints the reversed version of a string with charachter '-' in between.
+The program must preserve uppercase and lowercase letters.
+"""
+# reversed(sequence)
+
+def reverse_a_string_with_charachter(x):
+    reversed_word = "-".join(reversed(x))
+    return reversed_word
+
+reverse_a_string_with_charachter('hello test')
