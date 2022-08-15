fe3dcff5cf61c198f70f73fb1f07714143550de0
Najmeh Foroozani <najmeh.foroozani@gmail.com>
2022-08-15 08:23:00 +0200

    Delete 100_days_of_code_challenge_Python directory

--- 100_days_of_code_challenge_Python/day1.py
+++
@@ -1,8 +0,0 @@
-# 1. Write a Python program that prints the length of a strin
-#
-##
-def length_of_string(args):
-    print('{0} has lenght of'.format(args) ,len(args))
-
-##
-length_of_string('helljkhkjdhgaghdfghiuhforld')
